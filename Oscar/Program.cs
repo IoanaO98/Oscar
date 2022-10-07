107d84e9db703591f1377e6fbfc981a18f98d0fa
IoanaO98 <osan.ioana1998@gmail.com>
2022-10-07 15:58:59 +0300

    exercitii

--- Oscar/Program.cs
+++ Oscar/Program.cs
@@ -10,7 +10,32 @@ namespace Oscar
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("hello world");
+            //ex.5 
+            int number1, number2, temp;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + number1);
+            Console.Write("\nSecond Number : " + number2);
+            Console.Read();
+
+            //ex.1.Sharp to display the first 10 natural numbers. (for loop)
+            int x;
+            Console.WriteLine("Display the first 10 natural numbers; ");
+   
+            Console.WriteLine("The first 10 natural number are: ");
+            // for loop
+            for(x = 0; x <= 10; x++)
+            {
+                Console.WriteLine("{0}", x);
+            }
+
 		}
 	}
 }
+
